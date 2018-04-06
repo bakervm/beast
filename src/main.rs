dfcc66aa7bfa1ace29b223edca46419dcd39596b
Julian Laubstein <contact@julianlaubstein.de>
2018-04-06 22:34:26 +0200

    Implmemented proper cli for building + init

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ extern crate serde_derive;
 extern crate flate2;
 extern crate rmp_serde as rmps;
 extern crate toml;
+#[macro_use]
+extern crate structopt;
 
 mod ast;
 mod ast_gen;
@@ -21,7 +23,22 @@ mod parser;
 use compiler::Compiler;
 use config::Config;
 use melon::typedef::Result;
-use std::time::Instant;
+use std::{fs::{self, File}, io::Write, path::PathBuf, time::Instant};
+use structopt::StructOpt;
+
+const TARGET_DIRECTORY: &str = "target";
+const CONFIG_FILE_NAME: &str = "Beast.toml";
+
+#[derive(StructOpt)]
+enum Opt {
+    #[structopt(name = "init", about = "initialize a new Beast project at the given directory")]
+    Init {
+        #[structopt(help = "the path to the target project directory", parse(from_os_str))]
+        path: PathBuf,
+    },
+    #[structopt(name = "build", about = "builds the current project")]
+    Build,
+}
 
 fn main() {
     match run() {
@@ -34,7 +51,26 @@ fn main() {
 }
 
 fn run() -> Result<()> {
-    let config = Config::from_file("Beast.toml").unwrap();
+    let opt = Opt::from_args();
+
+    match opt {
+        Opt::Build => build()?,
+        Opt::Init { path } => init(&path)?,
+    }
+
+    Ok(())
+}
+
+fn build() -> Result<()> {
+    let config_file = PathBuf::from(CONFIG_FILE_NAME);
+
+    ensure!(
+        config_file.exists(),
+        "unable to find {} in current directory",
+        CONFIG_FILE_NAME
+    );
+
+    let config = Config::from_file(config_file)?;
 
     let entry_point = config
         .clone()
@@ -42,6 +78,8 @@ fn run() -> Result<()> {
         .unwrap_or_default()
         .entry_point
         .unwrap_or(ast_gen::BEAST_DEFAULT_ENTRY_POINT_MODULE.into());
+
+    let name = config.program.name.clone();
 
     let now = Instant::now();
 
@@ -51,6 +89,32 @@ fn run() -> Result<()> {
         "Compilation finished. Took {} seconds",
         now.elapsed().as_secs()
     );
+
+    let output_file = PathBuf::from(name).with_extension(melon::typedef::ROM_FILE_EXTENSION);
+    let output_path = PathBuf::from(TARGET_DIRECTORY).join(output_file);
+
+    program.save_as(output_path)?;
+
+    Ok(())
+}
+
+fn init(path: &PathBuf) -> Result<()> {
+    ensure!(!path.exists(), "directory already exists");
+
+    fs::create_dir_all(path.join(TARGET_DIRECTORY))?;
+    fs::create_dir_all(path.join(ast_gen::BEAST_DEFAULT_LIB_PATH))?;
+    fs::create_dir_all(path.join(ast_gen::BEAST_DEFAULT_INCLUDE_PATH))?;
+
+    let config_data = include_bytes!("templates/Beast.template.toml");
+    let main_file_data = include_bytes!("templates/main.bst");
+
+    let mut config_file = File::create(path.join(CONFIG_FILE_NAME))?;
+    config_file.write_all(&config_data[..])?;
+
+    let mut main_file_file = File::create(path.join("main.bst"))?;
+    main_file_file.write_all(&main_file_data[..])?;
+
+    println!("New project successfully initialized at {:?}", path);
 
     Ok(())
 }
