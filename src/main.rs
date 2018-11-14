9ccf1a94f508eba89adad90b139a72efd33cf10c
Julian Laubstein <contact@julianlaubstein.de>
2018-11-15 00:11:03 +0100

    Moved integration test into beast binary

--- src/main.rs
+++ src/main.rs
@@ -149,7 +149,8 @@ mod tests {
     extern crate tempfile;
 
     use self::assert_cmd::prelude::*;
-    use std::process::Command;
+    use melon::Program;
+    use std::{path::PathBuf, process::Command};
 
     #[test]
     fn init_compilation() {
@@ -168,5 +169,41 @@ mod tests {
             .arg("build")
             .assert()
             .success();
+    }
+
+    #[test]
+    fn compile_all_code_examples() {
+        let code_projects_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("code");
+
+        let code_dirs: Vec<_> = code_projects_dir
+            .read_dir()
+            .unwrap()
+            .filter(|path| path.is_ok())
+            .map(|path| path.unwrap().path())
+            .collect();
+
+        for code_dir in code_dirs {
+            Command::main_binary()
+                .unwrap()
+                .current_dir(&code_dir)
+                .arg("build")
+                .assert()
+                .success();
+
+            let target_dir = code_dir.join("target");
+
+            let rom_file = target_dir
+                .read_dir()
+                .unwrap()
+                .filter(|path| path.is_ok())
+                .map(|path| path.unwrap().path())
+                .find(|path| path.exists() && path.is_file())
+                .unwrap_or_else(|| {
+                    panic!("unable to find compilation artifact in {:?}", target_dir)
+                });
+
+            Program::from_file(&rom_file)
+                .unwrap_or_else(|_| panic!("unable to load program from file {:?}", rom_file));
+        }
     }
 }
